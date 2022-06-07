00982605e52f3d1a7585a9ba412c818b3137e2e9
glebignatski <61707825+glebignatski@users.noreply.github.com>
2022-06-07 18:51:55 -0400

    Update DuplicateFiles.java

--- DuplicateFiles.java
+++ DuplicateFiles.java
@@ -43,56 +43,64 @@ class DuplicateFiles {
         }
 		return lis;
     }
+    
+    public void printResult(String arg) {
+    	File folder = new File(arg);
+        List<String> file_names = listFilesForFolder(folder);
+        List<String> file_name_content_pair_array = new ArrayList<String>();
+        for (int i = 0; i < file_names.size(); i++) {
+        	
+    		  Path path = Paths.get(file_names.get(i));
+    		  int index = -1;
+    		  String current_name = new String();
+    		  current_name = file_names.get(i);
+    		  
+    		  index = current_name.length()-1;
+    		  
+    		  // Identify the name of the current file in the directory 
+    		  while (current_name.charAt(index) != '\\') {
+    			  index = index - 1;
+    		  }
+    		  
+    		  String path_no_file = new String();
+    		  path_no_file = current_name.substring(0, index);
+    		  String name = new String();
+    		  name = current_name.substring(index+1, current_name.length());
+    		  
+    		  String content = new String();
+    		  try {
+    	      content = Files.readString(path);
+    		  
+    		  } catch (IOException e) { e.printStackTrace(); }
+    		 //System.out.println();
+    		  
+    		 // Format the path, file name, and its content
+    		 file_name_content_pair_array.add(path_no_file + " " + name + "(" + content + ")");
+    		 	
+        }
+        // Display content of all parsed files in the directory
+        //System.out.println(file_name_content_pair_array);
+        //System.out.println("\n");
+        
+        List<List<String>> result = findDuplicate(file_name_content_pair_array);
+        if (result.size() == 0) {
+        	System.out.println("No duplicate files were detected.");
+        }
+        else {
+        	for (int i = 0; i < result.size(); i++) {
+        		System.out.println((i+1) + ". " + result.get(i));
+        	}
+        }
+    }
  
 public static void main(String[] args)  
 { 
     DuplicateFiles df = new DuplicateFiles();
+    for (int i = 0; i < args.length; i++) {
+    	System.out.println("\n-------------------------Results for Path " + (i+1) + ": " + args[i] + "-------------------------\n");
+    	df.printResult(args[i]);
+    	df.lis.clear();
+    }
     
-    File folder = new File(args[0]);
-    List<String> file_names = df.listFilesForFolder(folder);
-    List<String> file_name_content_pair_array = new ArrayList<String>();
-    for (int i = 0; i < file_names.size(); i++) {
-    	
-		  Path path = Paths.get(file_names.get(i));
-		  int index = -1;
-		  String current_name = new String();
-		  current_name = file_names.get(i);
-		  
-		  index = current_name.length()-1;
-		  
-		  // Identify the name of the current file in the directory 
-		  while (current_name.charAt(index) != '\\') {
-			  index = index - 1;
-		  }
-		  
-		  String path_no_file = new String();
-		  path_no_file = current_name.substring(0, index);
-		  String name = new String();
-		  name = current_name.substring(index+1, current_name.length());
-		  
-		  String content = new String();
-		  try {
-	      content = Files.readString(path);
-		  
-		  } catch (IOException e) { e.printStackTrace(); }
-		 //System.out.println();
-		  
-		 // Format the path, file name, and its content
-		 file_name_content_pair_array.add(path_no_file + " " + name + "(" + content + ")");
-		 	
-    }
-    // Display content of all parsed files in the directory
-    //System.out.println(file_name_content_pair_array);
-    //System.out.println("\n");
-    
-    List<List<String>> result = df.findDuplicate(file_name_content_pair_array);
-    if (result.size() == 0) {
-    	System.out.println("No duplicate files were detected.");
-    }
-    else {
-    	for (int i = 0; i < result.size(); i++) {
-    		System.out.println((i+1) + ". " + result.get(i));
-    	}
-    }
 } 
 }
